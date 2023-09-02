7532ab4ce719551b3d79b4389ec485a01e220db3
Y Rushik <111432352+RushikX@users.noreply.github.com>
2023-09-02 21:30:12 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import "package:flutter/material.dart";
+import 'package:expense_tracker/widgets/expenses.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 126, 203, 231),
+);
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 9, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 16,
+                ),
+              )),
+      debugShowCheckedModeBanner: false,
+      home: const Expenses(),
+    ),
+  );
+}
